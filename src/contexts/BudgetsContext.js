63df3a584baba298184659923958e226a5f207d2
Leighton <leighton.l14@gmail.com>
2022-04-01 01:04:06 +1100

    7th Commit - Added budget functions and useLocalStorage.js

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { v4 as uuidV4 } from "uuid";
 
 const BudgetsContext = React.createContext();
 
@@ -6,15 +7,51 @@ export function useBudgets() {
   return useContext(BudgetsContext);
 }
 
-export const BudgetsProvider = ({ children }) => {
-  const budgets = [];
-  const expenses = [];
+// {id:
+// name:
+// max}
 
-  function getBudgetExpenses() {}
-  function addExpense() {}
-  function deleteExpense() {}
-  function addBudget() {}
-  function deleteBudget() {}
+// {id:
+// budgetId:
+// amount:
+// description:}
+
+export const BudgetsProvider = ({ children }) => {
+  const [budgets, setBudgets] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  }
+
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+
+  function deleteExpense({ id }) {
+    //TODO: Deal with uncategorized expenses
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id);
+    });
+  }
+
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+
+  function deleteBudget({ id }) {
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id);
+    });
+  }
+
   return (
     <BudgetsContext.Provider
       value={{
