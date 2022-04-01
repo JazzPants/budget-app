39ce18a3bea7077e41da57cd0a830f7883eb0655
Leighton <leighton.l14@gmail.com>
2022-04-02 03:35:55 +1100

    21st Commit - Can delete expenses inside View Expenses

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -3,10 +3,13 @@ import {
   UNCATEGORIZED_BUDGET_ID,
   useBudgets,
 } from "../contexts/BudgetsContext";
+import { currencyFormatter } from "../utils";
 
 export default function ViewExpensesModal({ budgetId, handleClose }) {
   const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } =
     useBudgets();
+
+  const expenses = getBudgetExpenses(budgetId);
 
   const budget =
     UNCATEGORIZED_BUDGET_ID === budgetId
@@ -34,7 +37,25 @@ export default function ViewExpensesModal({ budgetId, handleClose }) {
           </Stack>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <Stack direction="vertical" gap="3">
+          {expenses.map((expense) => (
+            <Stack direction="horizontal" gap="2" key={expense.id}>
+              <div className="me-auto fs-4">{expense.description}</div>
+              <div className="me-auto fs-5">
+                {currencyFormatter.format(expense.amount)}
+              </div>
+              <Button
+                onClick={() => deleteExpense(expense)}
+                size="sm"
+                variant="outline-danger"
+              >
+                &times;
+              </Button>
+            </Stack>
+          ))}
+        </Stack>
+      </Modal.Body>
     </Modal>
   );
 }
